59a58400eb6d2cc0a159f38e6fadb9f63970e5c5
Joni Halabi <her@thatdevgirl.com>
2017-08-10 12:16:31 -0400

    Grabbing content from JSON object coming from D8

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gu-d8-events
+# Drupal 8 Events Manager
+
+This JS app is a proof of concept for an events manager using Drupal 8 as a backend. Setup instructions for the events content type and view to follow.
--- server.js
+++ server.js
@@ -1,11 +1,40 @@
+// Setting up the express app.
 const express = require('express');
 const app = express();
 const routes = require('./routes/index');
 
+// Setting up the D8 JSON export
+const request = require('request');
+const eventsUrl = 'http://localhost:8888/eventsexportall'; // obv not prod-ready.
+
+// Setting up the templating engine.
 app.set('view engine', 'ejs');
 
+// Starting the server.
 const server = app.listen(2000, () => {
   console.log('Listening on port 2000');
 });
 
+// Getting all of the events data
+request({
+  url: eventsUrl,
+  json: true
+}, function (err, res, body) {
+
+  // Exit if there is an error.
+  if (err || res.statusCode !== 200) {
+    console.error('An error has occurred.  Status code: ', res.statusCode);
+    return false;
+  }
+
+  // Create an array of the events
+  let events = [];
+  body.forEach((item) => {
+    events = events.concat(item);
+  });
+
+  app.locals.events = events;
+})
+
+// Calling the routes.
 app.use('/', routes);
